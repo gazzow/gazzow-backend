0388aeef93a69a33a4823d3cafcab30fcd44050f
Abbas firdous <abbasfathima40@gmail.com>
2025-09-14 20:14:07 +0530

    chore: update error message in login schema

--- src/application/validators/admin/auth.schema.ts
+++ src/application/validators/admin/auth.schema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  email: z.email({ error: "Invalid email format. Please try again" }),
+  email: z.email({ error: "Invalid email format" }),
   password: z
     .string()
     .min(6, { error: "Password must be at least 6 characters" }),
