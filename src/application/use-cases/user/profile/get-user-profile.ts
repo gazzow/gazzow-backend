58d719fd1aeebb99c1badfe937a4586f3f28da23
Abbas firdous <abbasfathima40@gmail.com>
2025-09-16 23:42:35 +0530

    refactor: add GetUserProfileUseCase interface

--- src/application/use-cases/user/profile/get-user-profile.ts
+++ src/application/use-cases/user/profile/get-user-profile.ts
@@ -3,12 +3,9 @@ import { AppError } from "../../../../utils/app-error.js";
 import { ResponseMessages } from "../../../../domain/enums/constants/response-messages.js";
 import { HttpStatusCode } from "../../../../domain/enums/constants/status-codes.js";
 import type { IUserRepository } from "../../../interfaces/repository/user-repository.js";
+import type { IGetUserProfileUseCase } from "../../../interfaces/user/profile/get-user-profile.js";
 
-export interface IGetUserProfileUC {
-  execute(id: string): Promise<IGetUserProfileResponseDTO>;
-}
-
-export class GetUserProfileUC implements IGetUserProfileUC {
+export class GetUserProfileUC implements IGetUserProfileUseCase {
   constructor(private userRepository: IUserRepository) {}
 
   execute = async (id: string): Promise<IGetUserProfileResponseDTO> => {
