c0d2229bd42ba7145b1393262b64f231c482ea40
Abbas firdous <abbasfathima40@gmail.com>
2026-02-05 23:12:29 +0530

    fix: global error handler issue fixed

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import connectDb from "./infrastructure/config/db.js";
 import http from "http";
 import { createSocketServer } from "./infrastructure/config/socket/socket-server.js";
 import { createRootRoutes } from "./presentation/routes/root.routes.js";
+import { errorHandler } from "./presentation/middleware/error-handler.js";
 
 const PORT = env.port;
 
@@ -14,6 +15,7 @@ connectDb().then(() => {
   const realtimeGateway = createSocketServer(server);
 
   app.use("/api", createRootRoutes(realtimeGateway));
+  app.use(errorHandler);
 
   server.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
