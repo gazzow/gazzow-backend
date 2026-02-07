9d1ae14730fb11158985b13904d49f69d6d7f134
Abbas firdous <abbasfathima40@gmail.com>
2026-02-07 22:26:04 +0530

    fix: create room when user online payload variable mix-match issue fixed

--- src/infrastructure/config/socket/register-socket.ts
+++ src/infrastructure/config/socket/register-socket.ts
@@ -2,10 +2,7 @@ import type { Server } from "socket.io";
 import { SOCKET_EVENTS } from "../../../domain/types/socket-events.js";
 import type { ISocketService } from "../../../application/providers/socket.service.js";
 
-export const registerSocket = (
-  io: Server,
-  socketService: ISocketService
-) => {
+export const registerSocket = (io: Server, socketService: ISocketService) => {
   io.on(SOCKET_EVENTS.CONNECTION, (socket) => {
     console.log("User connected:", socket.id);
 
@@ -16,14 +13,14 @@ export const registerSocket = (
 
     socket.on(SOCKET_EVENTS.USER_ONLINE, (payload) => {
       console.log("user online: ", payload);
-      socketService.joinUser(socket, payload.userId);
+      socketService.joinUser(socket, payload.id);
     });
 
     socket.on(
       SOCKET_EVENTS.SEND_MESSAGE,
       (data: { projectId: string; userId: string; content: string }) => {
         socketService.handleSendMessage(socket, data);
-      }
+      },
     );
 
     socket.on(
@@ -31,7 +28,7 @@ export const registerSocket = (
       (data: { userId: string }) => {
         console.log("Updating notification count for user:", data.userId);
         socketService.handleNotificationCountUpdate(socket, data.userId);
-      }
+      },
     );
   });
 };
