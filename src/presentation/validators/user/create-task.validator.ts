ff567b1f4f6731ea78ed9b34d07e9aa4b40699d2
Abbas firdous <abbasfathima40@gmail.com>
2026-02-19 20:17:16 +0530

    feat: add task create and update data validation using zod

---
+++ src/presentation/validators/user/create-task.validator.ts
@@ -0,0 +1,31 @@
+import z from "zod/v3";
+
+export const createTaskSchema = z.object({
+  title: z.string().min(3, "Title must be at least 3 characters long"),
+
+  description: z
+    .string()
+    .min(10, "Description must be at least 10 characters")
+    .max(699, "Description must not exceed 699 characters"),
+
+  assigneeId: z.string().optional(),
+
+  estimatedHours: z.coerce
+    .number({
+      invalid_type_error: "Estimated hours must be a number",
+    })
+    .min(1, "Estimated hours must be greater than 0"),
+
+  dueDate: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: "Please select a valid due date",
+    })
+    .refine((val) => new Date(val) > new Date(), {
+      message: "Due date must be in the future",
+    }),
+
+  priority: z.string().min(1, "Please select task priority"),
+
+  expectedRate: z.coerce.number().min(0).optional(),
+});
