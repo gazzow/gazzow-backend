81d1db1973b2b9a1776ee318f1883d9e0c0114c8
Abbas firdous <abbasfathima40@gmail.com>
2026-02-20 16:41:12 +0530

    feat: add create plan data validation

--- src/presentation/routes/admin/plan.routes.ts
+++ src/presentation/routes/admin/plan.routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { AdminDependencyContainer } from "../../../infrastructure/dependency-injection/admin-dependency-container.js";
 import { PlanDependencyContainer } from "../../../infrastructure/dependency-injection/admin/plan.container.js";
+import { validate } from "../../middleware/validate.middleware.js";
+import { createPlanSchema } from "../../validators/admin/create-plan.validator.js";
 
 const router = express.Router();
 
@@ -10,7 +12,12 @@ const verifyMiddleware = adminContainer.createVerifyAdminMiddleware();
 const planContainer = new PlanDependencyContainer();
 const planController = planContainer.createPlanController();
 
-router.post("/", verifyMiddleware.isAdmin, planController.createPlan);
+router.post(
+  "/",
+  verifyMiddleware.isAdmin,
+  validate(createPlanSchema),
+  planController.createPlan,
+);
 router.get("/", verifyMiddleware.isAdmin, planController.listPlan);
 
 router.get("/:planId", verifyMiddleware.isAdmin, planController.getPlan);
