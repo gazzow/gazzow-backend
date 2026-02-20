2b89f42206e4fbc96f81eb1c6f48402af1c7f9a9
Abbas firdous <abbasfathima40@gmail.com>
2026-02-20 21:10:11 +0530

    refactor: remove unused dto parameter

--- src/application/interfaces/usecase/admin/subscription/list-subscriptions.ts
+++ src/application/interfaces/usecase/admin/subscription/list-subscriptions.ts
@@ -1,10 +1,8 @@
 import type {
   IListSubscriptionsResponseDTO,
-  IListSubscriptionsRequestDTO,
 } from "../../../../dtos/admin/subscription.js";
 
 export interface IListSubscriptionsUseCase {
   execute(
-    dto: IListSubscriptionsRequestDTO
   ): Promise<IListSubscriptionsResponseDTO>;
 }
